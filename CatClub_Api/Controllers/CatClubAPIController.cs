767a3f5a548bcbc7dc070cb1dde3434b14bc2332
AdrianBielinski <adidosek@gmail.com>
2023-07-03 20:14:42 +0200

    Added controller and class

---
+++ CatClub_Api/Controllers/CatClubAPIController.cs
@@ -0,0 +1,20 @@
+﻿using CatClub_Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatClub_Api.Controllers
+{
+    [ApiController]
+    public class CatClubAPIController : ControllerBase
+    {
+        public IEnumerable<Cat> GetCats()
+        {
+            return new List<Cat>()
+            {
+new Cat{Id=0, Name="Pumczak", Age=1, Description="Pumczak to najgrubszy kot ze stada, lubi się bardzo przytulać i jest mocno zazdrosna."},
+new Cat{Id=1,Name="Kita", Age=1, Description="Kita jest najstarszą kocicą, to matka polka stada."},
+new Cat{Id=2, Name="Plamka", Age=1, Description="Plamka jest najmłodsza ze stada, była najbardziej schorowana. To troll."}
+            };
+
+        }
+    }
+}
---
+++ CatClub_Api/Models/Cat.cs
@@ -0,0 +1,11 @@
+﻿namespace CatClub_Api.Models
+{
+    public class Cat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Age { get; set; }
+        
+    }
+}
