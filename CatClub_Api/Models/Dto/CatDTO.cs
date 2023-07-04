c604635b4a480c08650fbc669763aad29bc7c72b
AdrianBielinski <adidosek@gmail.com>
2023-07-04 17:23:40 +0200

    Created CatDTO, method GetCatsDTO

---
+++ CatClub_Api/Models/Dto/CatDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CatClub_Api.Models.Dto
+{
+    public class CatDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
