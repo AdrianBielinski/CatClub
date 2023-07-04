d52fed94a70aab224b38ff6f91d12fa323475985
AdrianBielinski <adidosek@gmail.com>
2023-07-04 18:34:36 +0200

    Added same methods, but with ActionResult

--- CatClub_Api/Controllers/CatClubAPIController.cs
+++ CatClub_Api/Controllers/CatClubAPIController.cs
@@ -46,5 +46,45 @@ namespace CatClub_Api.Controllers
         {
             return CatStorage.catList.FirstOrDefault(x => x.Id == id);
         }
+
+        //Action Results
+
+        [Route("GetCats_ActionResult")]
+        [HttpGet]
+        public ActionResult<IEnumerable<Cat>> GetCats_ActionResult()
+        {
+            return new List<Cat>()
+            {
+                new Cat{Id=0, Name="Pumczak", Age=1, Description="Pumczak to najgrubszy kot ze stada, lubi się bardzo przytulać i jest mocno zazdrosna."},
+                new Cat{Id=1,Name="Kita", Age=1, Description="Kita jest najstarszą kocicą, to matka polka stada."},
+                new Cat{Id=2, Name="Plamka", Age=1, Description="Plamka jest najmłodsza ze stada, była najbardziej schorowana. To troll."}
+            };
+
+        }
+        [Route("GetCatsDTO_ActionResult")]
+        [HttpGet]
+        public ActionResult<IEnumerable<CatDTO>> GetCatsDTO_ActionResult()
+        {
+            return new List<CatDTO>
+            {
+                new CatDTO{Id=0, Name="Pumczak"},
+                new CatDTO{Id=1,Name="Kita"},
+                new CatDTO{Id=2, Name="Plamka"}
+            };
+        }
+
+        [Route("GetCatsDTO_LocalStorage_ActionResult")]
+        [HttpGet]
+        public ActionResult<IEnumerable<CatDTO>> GetCatsDTOFromStorage_ActionResult()
+        {
+            return CatStorage.catList;
+        }
+
+        [Route("GetCatsDTO_LocalStorage_GetCatById_ActionResult")]
+        [HttpGet("{id:int}")]
+        public ActionResult<CatDTO> GetCatsDTOFromStorage_ActionResult(int id)
+        {
+            return CatStorage.catList.FirstOrDefault(x => x.Id == id);
+        }
     }
 }
